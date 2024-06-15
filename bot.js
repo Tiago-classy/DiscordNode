f868b2190ad420f62182b40d98e23455a0bf3d5c
Tiago Ferraz <80392071+Tiago-classy@users.noreply.github.com>
2024-06-15 13:33:33 +0100

    CRONJOB CHANGE EVERY 6 HOURS

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.once('ready', () => {
     console.log('Ready!');
 
     // Schedule a task to send a message every 24 hours
-    cron.schedule('* * * * *', () => {
+    cron.schedule('0 6 * * *', () => {
         sendDailyMessage();
     });
 });
